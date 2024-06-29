f46ea4266f6f4a2c21e01db12958fcd89d57ed7d
Edward Heaver <edward.heaver01@gmail.com>
2024-06-29 16:44:14 +0100

    format and set some fields to omit if empty and cast to string

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Movie struct {
-	ID int64 `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	Title string `json:"title"`
-	Year int32 `json:"year"`
-	Runtime int32 `json:"runtime"`
-	Genres []string `json:"generes"`
-	Version int32 `json:"version"`
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty,string"`
+	Genres    []string  `json:"generes,omitempty"`
+	Version   int32     `json:"version"`
 }
