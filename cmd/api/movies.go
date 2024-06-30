266f0aab0b4321f3712664e9d03810a6eef3ebdb
Edward Heaver <edward.heaver01@gmail.com>
2024-06-30 16:29:53 +0100

    happy path create movie

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 	"time"
 
 	"github.com/captainmango/greenlight/internal/data"
 )
 
-
 func (a *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a movie")
+	var movie data.Movie
+
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		a.errorResponse(w, r, http.StatusBadRequest, err.Error())
+
+		return
+	}
+
+	err = a.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+
+	if err != nil {
+		a.logError(r, err)
+
+		a.serverErrorResponse(w, r, err)
+	}
 }
 
 func (a *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,13 +37,13 @@ func (a *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	movie := data.Movie{
-		ID: id,
+		ID:        id,
 		CreatedAt: time.Now(),
-		Title: "Up",
-		Year: 2011,
-		Runtime: 190,
-		Genres: []string{"kids", "comedy"},
-		Version: 1,
+		Title:     "Up",
+		Year:      2011,
+		Runtime:   190,
+		Genres:    []string{"kids", "comedy"},
+		Version:   1,
 	}
 
 	err = a.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
