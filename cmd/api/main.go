884c6ceca6c51893ec052922cd3c6f43a6882c47
Edward Heaver <edward.heaver01@gmail.com>
2025-03-30 18:42:16 +0100

    database set up is done. Added some config. Might want to break out the struct into it's own type

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,8 +13,8 @@ import (
 	// Import the pq driver so that it can register itself with the database/sql
 	// package. Note that we alias this import to the blank identifier, to stop the Go
 	// compiler complaining that the package isn't being used.
-	_ "github.com/lib/pq"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -24,7 +24,10 @@ type (
 		port int
 		env  string
 		db   struct {
-			dsn string
+			dsn          string
+			maxOpenConns int
+			maxIdleConns int
+			maxIdleTime  time.Duration
 		}
 	}
 
@@ -44,13 +47,18 @@ func main() {
 		logger.Error("Failed to read .env")
 		os.Exit(1)
 	}
-	
+
 	/*
 		Get vars passed in as flags and set on a struct to be read later. Defaults provided
 	*/
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", os.Getenv("ENVIRONMENT"), "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("PG_DSN"), "PostgreSQL DSN")
+	// Read the connection pool settings from command-line flags into the config struct.
+	// Notice that the default values we're using are the ones we discussed above?
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
 	flag.Parse()
 
 	db, err := openDB(cfg)
@@ -58,6 +66,11 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+
+	// Any of these being set to 0 will mean there is no limit
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
 
 	defer db.Close()
 	logger.Info("Established connection pool for database")
